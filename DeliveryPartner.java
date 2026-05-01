0c6652349b3264e649d5d32f9c773d166df74542
Swarada Deshpande <swaradadeshpande123@gmail.com>
2026-05-01 23:46:02 +0530

    Create DeliveryPartner.java

---
+++ DeliveryPartner.java
@@ -0,0 +1,5 @@
+// DeliveryPartner.java file of food delivery ecosystem small java application
+package users;
+public class DeliveryPartner extends User {  // Derived Class
+    public DeliveryPartner(String name) { super(name); }
+}
