42db903451066d51caaec8da24dfe58aba023222
Swarada Deshpande <swaradadeshpande123@gmail.com>
2026-05-01 23:44:55 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,6 @@
+// file of food delivery ecosystem small java application
+
+package users;
+public class Customer extends User { // Derived Class
+    public Customer(String name) { super(name); }
+}
