9c5dad7121c008a3f0056f5cfe245502361656eb
Swarada Deshpande <swaradadeshpande123@gmail.com>
2026-05-11 19:06:41 +0530

    Create Payment.java

---
+++ Payment.java
@@ -0,0 +1,6 @@
+package payments;
+public class Payment {
+    public void processPayment(double amount) {
+        System.out.println("Payment of " + amount + " processed successfully.");
+    }
+}
